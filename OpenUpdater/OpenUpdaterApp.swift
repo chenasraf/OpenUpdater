199a73e39ee72a1225568365b8a622d031b3582f
Chen Asraf <casraf@pm.me>
2026-06-20 23:29:36 +0300

    Initial Commit

---
+++ OpenUpdater/OpenUpdaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenUpdaterApp.swift
+//  OpenUpdater
+//
+//  Created by Chen Asraf on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenUpdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
